8d710ac4cc2fa38fc303cf7ac75ada3c5c2cf194
Hansey <meng.xu92@outlook.com>
2016-01-13 19:58:59 +0800

    V1.0
    Here is the V1.0 For only get result from specific Vpp and it runs on
    VS2010. However it did not support the Image File stream. For the Next
    Version, I will try to make it can get any result from a struct defined
    by user. The input string must be same as the Vpp output definition.
    P.S. The VisionPro version is 8.2

---
+++ VisionProAPI/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using VisionProAPI;
+
+namespace VisionProAPI
+{
+    public partial class Form1 : Form
+    {
+        Vision vision;
+        public double X = 0;
+        public double Y = 0;
+        public double Angle = 0;
+        public string waring = "";
+        public string pathin = @"D:\M503_Photo\1.bmp";
+        public string vpppath = @"D:\M503_Photo\M503.vpp";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vision.Run(ref waring,500,pathin);
+            vision.GetResult();
+            Angle = vision.Result_Angle;
+            X = vision.Result_X;
+            Y = vision.Result_Y;
+            label1.Text = X.ToString();
+            label2.Text = Y.ToString();
+            label3.Text = Angle.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vision = new Vision();
+            vision.init(vpppath);
+        }
+        
+    }
+}
---
+++ VisionProAPI/Vision.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Cognex.VisionPro.QuickBuild;
+using Cognex.VisionPro;
+using Cognex.VisionPro.Blob;
+using Cognex.VisionPro.ToolGroup;
+using Cognex.VisionPro.ImageFile;
+using Cognex.VisionPro.ResultsAnalysis;
+using Cognex.VisionPro.PMAlign;
+
+namespace VisionProAPI
+{
+    public class Vision
+    {
+        private CogJobManager myJobManager;
+        private CogJob myJob;
+        private CogJobIndependent myJobIndependent;
+        public double Result_X = 0;
+        public double Result_Y = 0;
+        public double Result_Angle = 0;
+        private CogImageFileTool mIFTool;
+        private Bitmap Imagein;
+        private CogToolGroup mTGTool;
+
+        public void init(string vpppath)
+        {
+            myJobManager = (CogJobManager)CogSerializer.LoadObjectFromFile(vpppath);
+            myJob = myJobManager.Job(0);
+            myJobIndependent = myJob.OwnedIndependent;
+            myJobManager.UserQueueFlush();
+            myJobManager.FailureQueueFlush();
+            myJob.ImageQueueFlush();
+            myJobIndependent.RealTimeQueueFlush();
+        }
+        public bool DataIn(string pathin)
+        {
+            mTGTool = (CogToolGroup)(myJobManager.Job(0).VisionTool);
+            mIFTool = (CogImageFileTool)(mTGTool.Tools["CogImageFileTool1"]);
+            mIFTool.Operator.Open(pathin, CogImageFileModeConstants.Read);            
+            Imagein = new Bitmap(pathin);
+            mIFTool.InputImage = new CogImage8Grey(Imagein);
+            mIFTool.Run();
+            return true; 
+        }
+        public bool GetResult()
+        {
+            if (myJobManager == null)
+            {
+                return false;
+            }
+            ICogRecord tmpRecord;
+            ICogRecord topRecord = myJobManager.UserResult();
+
+            if (topRecord == null)
+            {
+                return false;
+            }
+            
+            tmpRecord = topRecord.SubRecords[@"X"];
+            if (tmpRecord.Content != null)
+            {
+                Result_X = (double)tmpRecord.Content;
+            }
+            tmpRecord = topRecord.SubRecords[@"Y"];
+            if (tmpRecord.Content != null)
+            {
+                Result_Y = (double)tmpRecord.Content;
+            }
+            tmpRecord = topRecord.SubRecords[@"Angle"];
+            if (tmpRecord.Content != null)
+            {
+                Result_Angle = (double)tmpRecord.Content;
+            }
+            return true;
+        }
+        public bool Run(ref string exMessage,int time, string _pathin)
+        {
+            DataIn(_pathin);
+            try
+            {
+                myJobManager.Run();
+                System.Threading.Thread.Sleep(time);
+            }
+            catch (Exception ex)
+            {
+                exMessage = ex.Message;
+                return false;
+            }
+            return true;
+        }
+        public void Close()
+        {
+            myJob.Reset();
+            myJobManager.Stop();
+            myJobManager.Shutdown();
+            myJob = null;
+            myJobManager = null;
+            myJobIndependent = null;
+            GC.Collect();
+        }
+        internal bool RunContinuous(ref string exMessage)
+        {
+            try
+            {
+                myJobManager.RunContinuous();
+            }
+            catch (Exception ex)
+            {
+                exMessage = ex.Message;
+                return false;
+            }
+            return true;
+        }
+        internal void Stop()
+        {
+            myJobManager.Stop();
+        }
+    }
+}
